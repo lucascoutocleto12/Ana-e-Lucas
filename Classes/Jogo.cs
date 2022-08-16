f7132eb53b3e3a017d4fc875703510a7232788b6
lucascoutocleto12 <lucascoutocleto12@gmail.com>
2022-08-16 16:47:38 -0300

    Envio

---
+++ Classes/Jogo.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LPR___Ana_e_Lucas.Classes
+{
+    internal class Jogo
+    {
+
+        //Atributos
+        private String titulo; // Nome do Jogo
+        private int data; // Data de lançamento do Jogo
+        private String genero; // Gênero do Jogo
+        private int maxJogadores; // Máximo de Jogadores
+        private int metacritic; // Pontuação
+
+        //Construtores
+        public Jogo (String titulo, int data, String genero, int maxJogadores, int metacritic )
+        {
+            this.titulo = titulo;
+            this.data = data;
+            this.genero = genero;
+            this.maxJogadores = maxJogadores;
+            this.metacritic = metacritic;
+        }
+        public Jogo(String titulo, int data, String genero, int maxJogadores)
+        {
+            this.titulo = titulo;
+            this.data = data;
+            this.genero = genero;
+            this.maxJogadores = maxJogadores;
+        }
+
+        // Metodos de Acesso
+        public String getTitulo ()
+        {
+            return this.titulo;
+        }
+        public void setTitulo(String titulo)
+        {
+            if(titulo != "")
+            {
+                this.titulo = titulo;
+            }
+        }
+        public String getGenero()
+        {
+            return this.genero;
+        }
+        public void setGenero(String genero)
+        {
+            if (genero != "")
+            {
+                this.genero = genero;
+            }
+        }
+        public int getData()
+        {
+            return this.data;
+        }
+        public void setData(int data)
+        {
+            if (data > 1950)
+            {
+                this.data = data;
+            }
+        }
+        public int getMaxJogadores()
+        {
+            return this.maxJogadores;
+        }
+        public void setMaxJogadores(int maxJogadores)
+        {
+            if (maxJogadores >= 1)
+            {
+                this.maxJogadores = maxJogadores;
+            }
+        }
+        public int getMetacritic()
+        {
+            return this.metacritic;
+        }
+        public void setMetacritic(int metacritic)
+        {
+            if (metacritic >= 0 && metacritic <= 100 )
+            {
+                this.metacritic = metacritic;
+            }
+        }
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,132 @@
+﻿using LPR___Ana_e_Lucas.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LPR___Ana_e_Lucas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Criando página de entrada 
+
+            List<Jogo> ListadeJogo = new List<Jogo>();
+
+            while (true)
+            {
+
+
+
+                Console.WriteLine("======BIBLIOTECA DE JOGO======");
+                Console.WriteLine("");
+                Console.WriteLine("1 - ADICIONAR JOGO");
+                Console.WriteLine("2 - LISTAR JOGOS");
+                Console.WriteLine("3 - EDITAR JOGO");
+                Console.WriteLine("4 - REMOVER JOGO");
+                Console.WriteLine("");
+                Console.WriteLine("OPCAO: ");
+                String opcao = Console.ReadLine();
+
+                bool resultado;
+
+                if (opcao == "1")
+                {
+                    resultado = AdicionarJogo(ListadeJogo);
+
+                    if (resultado == true)
+                    {
+                        Console.WriteLine("");
+                        Console.WriteLine("Cadastrado com Sucesso!");
+                        Console.WriteLine("");
+                        Console.WriteLine("Pressione ENTER para continuar!");
+                        Console.ReadLine();
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Erro no Cadastro");
+                        Console.WriteLine("Pressione ENTER para continuar!");
+                        Console.ReadLine();
+                    }
+                }
+                else if (opcao == "2")
+                {
+                    resultado = ListarJogos(ListadeJogo);
+                }
+                else if (opcao == "4")
+                {
+                    resultado = DeletarJogo(ListadeJogo);
+
+                }
+
+            }
+
+        }
+        public static bool AdicionarJogo(List < Jogo > ListadeJogo)
+        {
+            Console.Clear();           
+            Console.Write("Titulo: ");
+            String titulo = Console.ReadLine();           
+            Console.WriteLine("");
+            Console.Write("Gênero: ");
+            String genero = Console.ReadLine();
+            Console.WriteLine("");
+            Console.Write("Data de Lançamento: ");
+            int data = Convert.ToInt32(Console.ReadLine());            
+            Console.WriteLine("");
+            Console.Write("Número Máximo de Jogadores: ");
+            int maxJogadores = Convert.ToInt32(Console.ReadLine());
+
+            if (titulo == "") return false;
+            if (genero == "") return false;
+            if (data < 1950) return false;
+            if (maxJogadores < 1) return false;
+
+            Jogo novojogo = new Jogo(titulo, data, genero, maxJogadores);
+
+            ListadeJogo.Add(novojogo);
+            Console.Beep();
+
+            return true;          
+        }
+
+        public static bool ListarJogos(List<Jogo> ListadeJogo)
+        {
+            Console.Clear();
+            Console.WriteLine("JOGOS ADICIONADOS: ");
+            Console.WriteLine("");
+
+            foreach (Jogo jogo in ListadeJogo)
+            {
+                Console.Write("Jogo: ");
+                Console.WriteLine(jogo.getTitulo());
+                Console.Write("Ano de lançamento: ");
+                Console.WriteLine(jogo.getData());
+                Console.Write("Gênero: ");
+                Console.WriteLine(jogo.getGenero());
+                Console.Write("Número máximo de jogadores: ");
+                Console.WriteLine(jogo.getMaxJogadores());            
+                Console.WriteLine("================================================================================================");
+                Console.WriteLine("");
+            }
+
+            Console.WriteLine("Pressione qualquer tecla para voltar ao menu...");
+            Console.ReadLine();
+            return true;
+        }
+        public static bool DeletarJogo(List<Jogo> ListadeJogo)
+        {
+            foreach (Jogo jogo in ListadeJogo)
+            { 
+                if()
+                {
+
+                }
+            }
+                return true;
+        }
+    }
+}
